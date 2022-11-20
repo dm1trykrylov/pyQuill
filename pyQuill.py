bd298db8c69d366bfefa0ae6d4bc40acf23723ad
dm1trykrylov <231014d@gmail.com>
2022-11-20 15:14:13 +0300

    Rename main file

--- pyQuill.py
+++
@@ -1,9 +0,0 @@
-import sys
-from main_window import MainWindow
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.setApplicationName("pyQuill")
-    window = MainWindow()
-    app.exec_()
